8b0a77f5e4dd15577f88f8259ea4a6cb7ea6ffdd
CGLemon <39462842+CGLemon@users.noreply.github.com>
2020-11-07 20:03:23 +0800

    remove GPL declaration

--- src/TemplateGo.cc
+++ src/TemplateGo.cc
@@ -21,13 +21,6 @@ static void selfplay_loop() {
 const static std::string get_License() {
 
     auto out = std::ostringstream{};
-
-    // out << "    ";
-    // out << PROGRAM << " " << VERSION << " Copyright (C) 2020  Hung-Zhe Lin"   << std::endl;
-    // out << "    This program comes with ABSOLUTELY NO WARRANTY."               << std::endl;
-    // out << "    This is free software, and you are welcome to redistribute it" << std::endl;
-    // out << "    under certain conditions; see the COPYING file for details."   << std::endl;
-
     return out.str();
 }
 
